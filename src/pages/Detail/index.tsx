6c5cb0e11ee7f29fb8b5d69228327e642e515ad6
daniel21h <hesseldaniel21@gmail.com>
2020-06-05 15:55:21 -0300

    feat: create detail screen

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { View, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-import style from './styles';
+
+import styles from './styles';
 
 const Detail = () => {
+  const navigation = useNavigation();
+
   return (
-    <View />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={24} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image
+          style={styles.pointImage}
+          source={{ uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }}
+        />
+
+        <Text style={styles.pointName}>Mercadão do João</Text>
+        <Text style={styles.pointItems}>Lampadas, Óleo de Cozinha</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Embu-Guaçu, SP</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button}>
+          <FontAwesome name="whatsapp" size={20} color="#fff" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button}>
+          <Icon name="mail" size={20} color="#fff" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   );
 }
 
