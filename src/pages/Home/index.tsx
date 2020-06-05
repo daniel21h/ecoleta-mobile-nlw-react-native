551d245019bbc2a69d4fcf19249c5208c653d1c7
daniel21h <hesseldaniel21@gmail.com>
2020-06-05 14:43:46 -0300

    feat: configuring navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { View, ImageBackground, Image, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 import backgroundImg from '../../assets/home-background.png'
 import logoImg from '../../assets/logo.png';
@@ -9,6 +10,8 @@ import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 const Home = () => {
+  const navigation = useNavigation();
+
   return (
     <ImageBackground 
       source={backgroundImg} 
@@ -26,7 +29,7 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={() => navigation.navigate('Points')}>
           <View style={styles.buttonIcon}>
             <Feather name="arrow-right" color="#fff" size={24} />
           </View>
