6b2db22f14f02d3c3c5f6bce45b89fd8fd3d5df4
daniel21h <hesseldaniel21@gmail.com>
2020-06-05 14:26:59 -0300

    feat: create home screen

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { View, ImageBackground, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import backgroundImg from '../../assets/home-background.png'
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
+
+const Home = () => {
+  return (
+    <ImageBackground 
+      source={backgroundImg} 
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={logoImg} />
+        <Text style={styles.title}>
+          Seu marketplace de coleta de resíduos
+        </Text>
+        <Text style={styles.description}>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Feather name="arrow-right" color="#fff" size={24} />
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  )
+}
+
+export default Home;
+
